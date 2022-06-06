915fa31bc1339ea1d84a699877cf7a16134127d7
Tony Greenberg <info@bayesicresearch.org>
2022-06-06 12:26:17 -0400

    modify display setting script for the P1

--- systemConf/displaySetup.sh
+++ systemConf/displaySetup.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 # check if the NVIDIA GPU is active and tell xorg to use it for the dsiplay
 # this script goes into /etc/lightdm
-xrandr --setprovideroutputsource modesetting NVIDIA-0
 xrandr --output $( xrandr | grep eDP | awk '{print $1}' ) --auto --primary --dpi 144
-if [ $( xrandr | grep HDMI | awk '{print $2}' ) == "connected" ]; then 
-	output=$( xrandr | grep HDMI | awk '{print $1}' )
-	xrandr --output $output --auto --left-of eDP-1-1
+if [ $( xrandr | grep '^DP1' | awk '{print $2}' ) == "connected" ]; then 
+	output=$( xrandr | grep '^DP1' | awk '{print $1}' )
+	xrandr --output $output --auto --right-of eDP1
 fi
-if [ $( xrandr | grep '^DP-1' | awk '{print $2}' ) == "connected" ]; then 
-	output=$( xrandr | grep '^DP-1' | awk '{print $1}' )
-	xrandr --output $output --auto --right-of eDP-1-1
+if [ $( xrandr | grep '^DP2' | awk '{print $2}' ) == "connected" ]; then 
+	output=$( xrandr | grep '^DP2' | awk '{print $1}' )
+	xrandr --output $output --auto --left-of eDP1
 fi
-if [ $( xrandr | grep '^DP-0' | awk '{print $2}' ) == "connected" ]; then 
-	output=$( xrandr | grep '^DP-0' | awk '{print $1}' )
-	xrandr --output $output --auto --right-of eDP-1-1
+if [ $( xrandr | grep '^DP3' | awk '{print $2}' ) == "connected" ]; then 
+	output=$( xrandr | grep '^DP3' | awk '{print $1}' )
+	xrandr --output $output --auto --left-of eDP1
 fi
 
